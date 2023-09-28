98508498cfe4b2941f08c640283d60c1342d6691
marcoder2 <marcos.domingos02@gmail.com>
2023-09-28 16:07:47 -0300

    alterações no arquivos changeTheme.ts, criação do texto fixo de botão Mudar o Tema e criação das classes no Css de temas Claro e Escuro

--- src/changeTheme.ts
+++ src/changeTheme.ts
@@ -1,9 +1,6 @@
 export function changeTheme(element: HTMLButtonElement) {
-  let counter = 0
-  const setCounter = (count: number) => {
+  const setThemes = () => {
     counter = count
-    element.innerHTML = `Mudar o Tema`
   }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+  element.addEventListener('click', () => setThemes())
 }
