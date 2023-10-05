ab2c9ba0e5cdbbd0ef0e0147faf3007181baf68a
marcoder2 <marcos.domingos02@gmail.com>
2023-10-05 15:14:09 -0300

    Lógica de mudança de tema adicionado ao arquivo changeTheme.ts e ajuste nas classes de theme-light e theme-dark para a cor do texto estar o oposto da cor de fundo

--- src/changeTheme.ts
+++ src/changeTheme.ts
@@ -1,6 +1,14 @@
 export function changeTheme(element: HTMLButtonElement) {
+  let body = document.querySelector('body')
   const setThemes = () => {
-    counter = count
+    const classList = body?.classList
+    if(classList?.contains("theme-light")){
+      classList.remove("theme-light");
+      classList.add("theme-dark")
+    } else {
+      classList?.remove("theme-dark")
+      classList?.add("theme-light")
+    }
   }
   element.addEventListener('click', () => setThemes())
 }
